97cef5d22799b4a66c46157af358313e690c133c
Jake Omann <jomann@nagios.com>
2014-07-28 13:04:04 -0500

    Added wget to dependencies

--- prereq-install.sh
+++ prereq-install.sh
@@ -6,10 +6,10 @@ then
 	echo "must be installed manually"
 elif uname -a | grep -i 'el5';
 then
-	yum install gcc zlib zlib-devel openssl openssl-devel rpm-build -y
+	yum install gcc zlib zlib-devel openssl openssl-devel rpm-build wget -y
 elif  uname -a | grep -i 'el6';
 then
-	yum install gcc zlib zlib-devel openssl openssl-devel rpm-build -y
+	yum install gcc zlib zlib-devel openssl openssl-devel rpm-build wget -y
 else
 	echo "Could not determine your OS type... you need to install the following dependencies:"
 	echo ""
